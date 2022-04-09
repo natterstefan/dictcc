ef7214fb20e34e08a42b0c5152126941b5ff48be
Stefan Natter <stefan@natter.at>
2022-04-09 06:50:15 +0200

    chore: removed dist

--- dist/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default } from './dictcc';
-export { Languages } from './config';
-export type { Translation, TranslationResult } from './types';
--- dist/types/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import { TranslationInput } from './types';
-export declare const getTextMeta: (text: string) => string[] | undefined;
-export declare const getTranslatedText: (text: string) => string;
-export declare const getDictccUrl: ({ fromLang, toLang, term }: TranslationInput) => string;
